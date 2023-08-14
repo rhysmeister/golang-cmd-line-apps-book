93d5b9c9296d03c10ba8474c0842ab0e727706a3
Rhys Campbell <rhys.james.campbell@googlemail.com>
2023-08-14 20:37:44 +0200

    Todo server test working

--- apis/todoServer/go.mod
+++ apis/todoServer/go.mod
@@ -2,6 +2,6 @@ module github.com/rhysmeister/apis/todoServer
 
 go 1.20
 
-require github.com/rhysmeister/apis/interacting/todo v0.0.0
+require github.com/rhysmeister/interacting/todo v0.0.0
 
-replace github.com/rhysmeister/apis/interacting/todo => ../../interacting/todo
+replace github.com/rhysmeister/interacting/todo => ../../interacting/todo
---
+++ interacting/todo/cmd/todo/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/rhysmeister/interacting/todo"
+)
+
+var todoFileName = ".todo.json"
+
+func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Book Exercise tool\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage Information:\n")
+		flag.PrintDefaults()
+	}
+
+	add := flag.Bool("add", false, "Add a task to the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	del := flag.Int("del", 0, "Item to be deleted")
+	verbose := flag.Bool("v", false, "Enable verbose output")
+	flag.Parse()
+
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
+
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		if *verbose {
+			fmt.Print(verboseOutput(l))
+		} else {
+			fmt.Print(l)
+		}
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *add:
+		tasks, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		for _, t := range tasks {
+			l.Add(t)
+		}
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *del > 0:
+		if err := l.Delete(*del); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
+
+func getTask(r io.Reader, args ...string) ([]string, error) {
+	if len(args) > 0 {
+		return []string{strings.Join(args, " ")}, nil
+	}
+
+	tasks := make([]string, 0)
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		if err := s.Err(); err != nil {
+			return nil, err
+		}
+		if len(s.Text()) == 0 {
+			return nil, fmt.Errorf("task cannot be blank")
+		}
+		tasks = append(tasks, s.Text())
+	}
+	return tasks, nil
+}
+
+func verboseOutput(l *todo.List) string {
+	formatted := ""
+
+	for k, t := range *l {
+		prefix := "  "
+		if t.Done {
+			prefix = "X "
+		}
+		formatted += fmt.Sprintf("%s%d: %s %s %s\n", prefix, k+1, t.Task, t.CreatedAt, t.CompletedAt)
+	}
+	return formatted
+}
